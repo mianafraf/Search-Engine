70f87a031b3865335bfd0723a3af03a4aec328d0
Mian Afrasiyyab Farakh <mianafraf@gmail.com>
2021-12-09 18:41:51 +0500

    commit

--- generate.py
+++ generate.py
@@ -2,25 +2,44 @@ import csv
 
 
 class generate:
-
-    def __init__(self, dindex, windex):
-        self.dindex = dindex
-        self.windex=windex
-
-    def loadlexicon():
-     lexicon={}
+      
+    def __init__(self):
+     f=open('index.txt','r')
+     data=f.readline()
+     lis=data.split(",")
+     self.wid=int(lis[0])
+     self.did=int(lis[1])
+     self.lexicon={}
+     self.doc={}
+     f.close()
+  
+        
+    def loadlexicon(self):
      with open('lexicon.csv','r') as data:
       for line in csv.reader(data):
             k,d=line
-            lexicon[k]=d
-     return lexicon
+            self.lexicon[k]=d
     
-    def savelexicon(lexicon):
+    def savelexicon(self):
      with open('lexicon.csv', 'w') as f:
-      for key in lexicon.keys():
-        f.write("%s,%s\n"%(key,lexicon[key]))
+      for key in self.lexicon.keys():
+        f.write("%s,%s\n"%(key,self.lexicon[key]))
     
 
+    def generatelexicon(self,arr):
+     for i in arr:
+       if len(i[0])>1:
+        word=i[0]
+        if word in self.lexicon.keys():
+        #generate forward index here
+         pass
+        else:
+         self.wid=self.wid+1
+         self.lexicon[word]=self.wid
+         self.generatelexicon(word)
 
-
-   # def generatelexicon(a):
+    def saveindex(self):
+      f=open('index.txt','w')
+      f.write("%d,%d\n"%(self.wid,self.did))
+      
+      
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import generate
+from generate import generate
 import csv
-f=open('forwardindex.csv','w')
 
-f.close()
+x=generate()
+x.loadlexicon()
+row,col=5,2
+a = [[0 for i in range(col)] for j in range(row)]
+for i in a:
+ val=input("Enter word: ")
+ print(val)
+ i[0]=val
+
+print(a)
+x.generatelexicon(a)
+x.savelexicon()
+x.saveindex()
 
