3390abe5e1ca047b5995be791fd962a586d25961
Ahmad Tashfeen <67421952+ahmadtash7@users.noreply.github.com>
2021-12-15 17:43:00 +0500

    Add files via upload

---
+++ parsing2.py
@@ -0,0 +1,76 @@
+from nltk.tokenize import word_tokenize
+import pandas as pd
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from generate import generate
+import os
+print(os.getcwd())
+
+#basePath = os.path.dirname(os.path.abspath(__file__))
+basePath = os.getcwd()
+#df = pd.read_json(basePath + '/nela-covid-2020/newsdata/21stcenturywire.json')
+df = pd.read_json(open('21stcenturywire.json', "r", encoding="utf8"))
+# parsing json file to pd dataframe
+#df = pd.read_json('nela-covid-2020/newsdata/21stcenturywire.json')
+
+# initializes dictionary where key is a word in the article
+# the values for each key are the positions at which they occur in the doc
+# number of occurences can be obtained by using len()
+
+v = generate()
+print("start")
+
+stop_words = set(stopwords.words("english"))
+
+lemmatizer = WordNetLemmatizer()
+
+
+path = "/Users/ahmadtashfeen/Downloads/dataverse_files/nela-covid-2020/newsdata"
+
+filenames = []
+
+for f in os.listdir(path):
+    filenames.append(f)
+
+v.loadlexicon()
+i = 0
+
+for f in filenames:
+    for item in range(0, len(df.index)):
+        occ_dict = {}
+        # obtaining content from dataframe
+        # placing content in array
+        words = df['content'][item].split()
+
+        # code for getting indices of words
+        
+        for index, word in enumerate(words):
+            if len(word) > 1:
+                if word not in stop_words:
+                    emptyString = ""
+                    word = word.lower()
+                    if word.isalpha():
+                        for character in word:
+                            if character.isalnum():
+                                emptyString += character
+                            else:
+                                emptyString += " "
+                        if word in occ_dict.keys():
+                            occ_dict[word].append(index)
+                        else:
+                            occ_dict[word] = []
+                            occ_dict[word].append(index)
+                    v.generatelexicon(occ_dict)
+                else:
+                    break
+        # if(i==0):print(occ_dict)
+        i = i+1
+        print(i)
+    print(f)
+    v.savelexicon()
+    v.savefindex()
+    v.saveindex()
+    v.saveinvindex()
+
+print("stop")
