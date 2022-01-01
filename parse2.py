ca760a2c9c9da6213f0294ae78dc966b17867f1d
Mian Afrasiyyab Farakh <mianafraf@gmail.com>
2022-01-01 15:04:28 +0500

    commit

---
+++ parse2.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import csv
+from generate import generate
+from generate import gen
+
+import os
+print(os.getcwd())
+
+v=gen()
+
+#basePath = os.path.dirname(os.path.abspath(__file__))
+basePath=os.getcwd()
+#df = pd.read_json(basePath + '/nela-covid-2020/newsdata/21stcenturywire.json')
+df=pd.read_json(open('21stcenturywire.json', "r", encoding="utf-8"))
+#parsing json file to pd dataframe
+#df = pd.read_json('nela-covid-2020/newsdata/21stcenturywire.json')
+
+#initializes dictionary where key is a word in the article
+#the values for each key are the positions at which they occur in the doc
+#number of occurences can be obtained by using len() 
+
+
+print("welcome")
+
+
+for item in range(0,len(df.index)):
+    occ_dict = {}
+    #obtaining content from dataframe
+    #placing content in array
+    words = df['content'][item].split()
+    title=df['title'][item]
+    url=df['url'][item]
+
+    #code for getting indices of words
+    v.loadlexicon()
+    v.loadfindex()
+    v.loadinvindex()
+    v.loaddocids()
+    v.loadfilesread()
+    #v.loaddocids()
+    i=0
+    for index,word in enumerate(words):
+
+        if len(word)>2:
+            if word in occ_dict.keys():
+
+                occ_dict[word].append(index)
+            else:
+                occ_dict[word] = []
+                occ_dict[word].append(index)
+    
+    
+    v.generatelexicon(occ_dict,title,url)
+    #if(i==0):print(occ_dict)
+    i=i+1
+    print(i)
+    print(v.doc)
+    v.sortfullinvindex()
+    v.savelexicon()
+    v.savefindex()
+    v.saveinvindex()
+    v.savedocids()
+    v.saveindex()
+    v.savefilesread()
